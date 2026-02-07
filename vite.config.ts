fcb83f1a78c2da7bee3cdbfbe8adb21f01f7bd4c
Rithick2024 <rithick.r@datamoo.ai>
2026-02-07 23:28:50 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Teddy-Day-Wishes-Template/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
